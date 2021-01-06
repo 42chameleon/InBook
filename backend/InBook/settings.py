a1ccb180bad91dc883d2a341b96d7c7f19c8e6a8
Muhammad Israpilov <42israpilov@mail.ru>
2021-01-06 15:22:16 +0300

    Created models Category, Book, UserBookRelation, Profile

--- backend/InBook/settings.py
+++ backend/InBook/settings.py
@@ -15,18 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'InBook.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +82,6 @@ DATABASES = {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
---
+++ backend/book/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'ID {self.id}: {self.name}'
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    author_name = models.CharField(max_length=300)
+    book_file = models.FileField(upload_to='uploads/', blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_book')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'ID {self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False, blank=True)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return f' user: {self.user}, {self.book}, rate: {self.rate}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='book', on_delete=models.SET_NULL, null=True)
+    relation = models.ForeignKey(UserBookRelation, on_delete=models.SET_NULL, null=True)
